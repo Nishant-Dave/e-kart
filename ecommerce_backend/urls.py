877bf307ad4304f5a96dd60ab41677c37a3016cd
Nishant-Dave <nishant.nsbda@gmail.com>
2026-03-30 00:05:03 +0530

    minor bug fix (home API endpoint added)

--- ecommerce_backend/urls.py
+++ ecommerce_backend/urls.py
@@ -3,11 +3,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import health_check
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Backend is running 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/health/', health_check, name='health_check'),
-    
+    path('', home),
+
     # App-level routing
     path('api/auth/', include('users.urls', namespace='users')),
     path('api/', include('products.urls', namespace='products')),
--- ecommerce_frontend/src/services/api.js
+++ ecommerce_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://e-kart-3xsq.onrender.com/api/',
 });
 
 // Configure Axios to automatically attach JWT token on requests
